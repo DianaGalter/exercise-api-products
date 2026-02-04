8e28715d5c71ac1f5ad7d9bc2ef20164f1fc8116
DianaGalter <diana.bobrova.spb@gmail.com>
2026-02-04 17:31:48 +0200

    create server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import productsRoute from "./routes/products.route.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/products", productsRoute);
+
+app.use("/", (req, res) => {
+  res.send("fallback..404 - not found");
+});
+
+app.listen(3000);
